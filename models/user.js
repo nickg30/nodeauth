38dd5a46bc7a1e20a96495cc1e36e32c521fcf7c
Nicholas Guerra <nick@scriptthread.com>
2017-12-02 23:32:13 -0800

    authentication and bcrypt added

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
 
 mongoose.connect('mongodb://nguerra:david3guerra@ds127936.mlab.com:27936/node-auth', {
     useMongoClient: true,
@@ -29,6 +30,26 @@ var UserSchema = mongoose.Schema({
 
 var User = module.exports = mongoose.model('User', UserSchema);
 
+module.exports.getUserById = function(id, callback) {
+    User.findById(id, callback);
+};
+
+module.exports.getUserByUsername = function(username, callback) {
+    var query = {username: username};
+    User.findOne(query, callback);
+};
+
+module.exports.comparePassword = function(candidatePassword, hash, callback) {
+    bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+        callback(null, isMatch); 
+    });
+};
+
 module.exports.createUser = function(newUser, callback) {
-    newUser.save(callback);
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(newUser.password, salt, function(err, hash) {
+            newUser.password = hash;
+            newUser.save(callback); 
+        });
+    });
 }
